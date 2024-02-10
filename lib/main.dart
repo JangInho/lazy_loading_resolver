25e4d90162407e7fbaac494cacbe3bf083a3d31f
“JangInho” <wkddlsgh50@naver.com>
2024-02-10 21:12:04 +0900

    feat: Scrollable.ensureVisible() test

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -28,9 +30,54 @@ class LazyLoadingResolver extends StatefulWidget {
 }
 
 class _LazyLoadingResolverState extends State<LazyLoadingResolver> {
+  TextEditingController textEditingController = TextEditingController();
+  List<GlobalKey> keys = List.generate(50, (index) => GlobalKey());
+
   @override
   Widget build(BuildContext context) {
-    return ;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('lazy loading 을 없애보자!!'),
+      ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                    controller: textEditingController,
+                    keyboardType: TextInputType.number),
+              ),
+              Expanded(
+                  child: ElevatedButton(
+                      onPressed: () {
+                        log("scroll to target!!!!");
+                        if (keys[int.parse(textEditingController.text)]
+                                .currentContext ==
+                            null) {
+                          log("fail scroll to");
+                          return;
+                        }
+                        Scrollable.ensureVisible(
+                            keys[int.parse(textEditingController.text)]
+                                .currentContext!,
+                            duration: const Duration(microseconds:500 ));
+                      },
+                      child: const Text("start")))
+            ],
+          ),
+          Expanded(
+            child: ListView.builder(
+                itemCount: 50,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    key: keys[index],
+                    title: Text('😄 $index'),
+                  );
+                }),
+          ),
+        ],
+      ),
+    );
   }
 }
-
